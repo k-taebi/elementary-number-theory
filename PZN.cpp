953b86566b599fa34ecb55b396ed3080e4e15240
Korosh Taebi <k.taebi@gmail.com>
2024-02-13 23:03:15 +0100

    add method to replace digits and check PZN

--- PZN.cpp
+++ PZN.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -19,10 +20,31 @@ int calculate_PZN(int PZN)
     return weightedSum % 11;
 }
 
+int replace_digit(int num, int x, int y)
+{
+    std::string num_str = std::to_string(num);
+
+    char char_x = '0' + x;
+    char char_y = '0' + y;
+
+    std::replace(num_str.begin(), num_str.end(), char_x, char_y);
+
+    return stoi(num_str);
+}
+
 int main()
 {
-    int number = 435713;
-    cout << "PZN = " << calculate_PZN(number) << endl;
+    int number = 187377;
+    int test_PZN = calculate_PZN(number);
 
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+        {
+            int new_number = replace_digit(number, i, j);
+            if (calculate_PZN(new_number) == test_PZN)
+            {
+                cout << i << " " << j << endl;
+            }
+        }
     return 0;
 }
