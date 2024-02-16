494ddc208fab96642227dd314a237a7d193282c0
Korosh Taebi <korosh_taebi@deichmann.com>
2024-02-16 21:04:20 +0100

    add gcd header file

---
+++ gcd.h
@@ -0,0 +1,10 @@
+#ifndef GCD_H
+#define GCD_H
+/**
+ * Calculate the greatest common divisor of two integers.
+ * @param a first integer
+ * @param b second integer
+ * @return greatest common divisor of a and b
+ */
+long gcd(long a, long b);
+#endif
